17448279a38301a8c0f0aa6dfcdf6d99a4f287f1
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 17:41:28 +0700

    add generator

--- blocks/blocks_laser-emit.js
+++ blocks/blocks_laser-emit.js
@@ -1,15 +1,19 @@
-Blockly.Blocks['ky_008_laser_emit_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-008 LASER EMIT");
-    this.appendValueInput("KY_008_LASER_EMIT_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_008_laser_emit_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-008 LASER EMIT");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_008_LASER_EMIT_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_008_LASER_EMIT_STATE"),
+          "KY_008_LASER_EMIT_STATE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_laser-emit.js
+++ blocks/generators_laser-emit.js
@@ -1,6 +1,22 @@
-Blockly.JavaScript['ky_008_laser_emit_block'] = function(block) {
-  var value_ky_008_laser_emit_pin = Blockly.JavaScript.valueToCode(block, 'KY_008_LASER_EMIT_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_008_laser_emit_block"] = function(block) {
+    var text_ky_008_laser_emit_pin = block.getFieldValue(
+      "KY_008_LASER_EMIT_PIN"
+    );
+    var variable_ky_008_laser_emit_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_008_LASER_EMIT_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	int KY_008_LASER_EMIT_PIN = ${text_ky_008_laser_emit_pin};
+	#END
+
+	#SETUP
+		pinMode(KY_008_LASER_EMIT_PIN, OUTPUT);
+	#END
+	
+	${variable_ky_008_laser_emit_state} = KY_008_LASER_EMIT_PIN;
+    `;
+    return code;
+  };
